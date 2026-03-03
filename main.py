6a363856e973973b61221f028f2e6002a4b2a01d
rockblinds-lang <rockblinds@gmail.com>
2026-03-03 15:07:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_analytics2 import track
 
 # Початок обгортки (без відступу)
-with track(password="vash_parol123"):
+with track(unsafe_password="Pro_Tyre"):
     # ТУТ ПІШОВ ВІДСТУП (4 пробіли або Tab) ДЛЯ ВСЬОГО КОДУ:
     st.markdown(
         """
