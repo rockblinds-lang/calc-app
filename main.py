6607736c13a12b7932faf13a531fea91c3fc7907
rockblinds-lang <rockblinds@gmail.com>
2026-03-03 14:36:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import streamlit.components.v1 as components  # Додайте цей рядок теж
-
+from streamlit_analytics2 import track # Додаємо це
 # Ось сюди ми "вклинюємо" код аналітики:
 st.markdown(
     """
